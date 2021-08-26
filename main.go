10c391d00de9502bf8215881fb3dac567a6827a1
ankursinghfoodpanda <ankur.singh@foodpanda.com>
2021-08-26 12:00:33 +0800

    Init commit

---
+++ main.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"sync"
+
+	"github.com/aws/aws-sdk-go/aws"
+)
+
+var prPool = sync.Pool{
+	New: func()interface{} {
+
+		return new(DBConnection)
+	},
+}
+
+
+type Config struct{
+	URL string
+	Name string
+}
+
+type DBConnection struct{
+	db * DBConnection
+	config Config
+}
+
+func New() * DBConnection{
+	return &DBConnection{
+		db:     &DBConnection{},
+		config: aws.Config{},
+	}
+}
+
+func (d DBConnection) Fetch() string{
+	return "fetched data"
+}
+
+func main() {
+	//body := bytes.NewBuffer([]byte{})
+	//req, err := http.NewRequest(http.MethodGet, "http://golang.tutorial.org", body)
+	//if err != nil{
+	//	panic(err)
+	//}
+	http.HandleFunc("/", handler)
+}
+
+
+// methods to initialise the database connections
+
+func handler(w http.ResponseWriter, r * http.Request){
+	// db = DBConnectionP{}
+	db, ok := prPool.Get().(* DBConnection)
+	prPool.Put(db)
+	// initialise the state of the db
+	if !ok{
+		fmt.Fprintf(w, "wrong data type")
+	}
+	output := db.Fetch()
+	// reset the state of the db object
+	fmt.Fprintf(w, output)
+}
